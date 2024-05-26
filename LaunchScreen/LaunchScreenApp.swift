2de1bdec3d854d0f76b06cf6af5cc2b623da978b
Julio-Cesar-Lopez <cesarhashibira@gmail.com>
2024-05-26 13:54:11 -0600

    Initial Commit

---
+++ LaunchScreen/LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenApp.swift
+//  LaunchScreen
+//
+//  Created by Julio Cesar on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
